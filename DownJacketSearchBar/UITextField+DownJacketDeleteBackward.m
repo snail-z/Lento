b2235376bc1eb081093c5840cbe1fa4d5782e7ef
Zhang <40460770@qq.com>
2016-11-18 11:31:24 +0800

    Update UITextField+DownJacketDeleteBackward.m

--- DownJacketSearchBar/UITextField+DownJacketDeleteBackward.m
+++ DownJacketSearchBar/UITextField+DownJacketDeleteBackward.m
@@ -20,11 +20,19 @@ NSString * const DownJacketTextFieldDidDeleteBackwardNotification = @"com.zhang.
 }
 
 - (void)downJacketdeleteBackward {
-    [self downJacketdeleteBackward];
     if ([self.delegate respondsToSelector:@selector(textFieldDidDeleteBackward:)]) {
         id <DownJacketTextFieldDelegate> delegate  = (id<DownJacketTextFieldDelegate>)self.delegate;
         [delegate textFieldDidDeleteBackward:self];
     }
+    /**
+     1. 交互方法:runtime
+     method_exchangeImplementations(deleteBackward, downJacketdeleteBackward);
+     也就是外部调用downJacketdeleteBackward就相当于调用了deleteBackward，调用deleteBackward就相当于调用了downJacketdeleteBackward
+     2. 此时调用的方法 'downJacketdeleteBackward' 相当于调用系统的 'deleteBackward' 方法,原因是在load方法中进行了方法交换.
+     3. 注: 此处并没有递归操作
+     4. 在执行自定义方法'downJacketdeleteBackward'之后再去调用'deleteBackward'，可以防止多余删除操作！（可根据实际情况处理调用的先后顺序）
+     */
+    [self downJacketdeleteBackward];
     [[NSNotificationCenter defaultCenter] postNotificationName:DownJacketTextFieldDidDeleteBackwardNotification object:self];
 }
 
