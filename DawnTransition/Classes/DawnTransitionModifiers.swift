882294f30bc6474fc398daf60e6a9634b9b3d1a4
zhang <40460770@qq.com>
2023-08-18 14:07:45 +0800

    update

--- DawnTransition/Classes/DawnTransitionModifiers.swift
+++ DawnTransition/Classes/DawnTransitionModifiers.swift
@@ -3,28 +3,8 @@
 //  DawnTransition
 //
 //  Created by zhang on 2022/6/12.
+//  Copyright (c) 2022 snail-z <haozhang0770@163.com> All rights reserved.
 //
-//  The MIT License (MIT)
-//
-//  Copyright (c) 2022 snail-z <haozhang0770@163.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
 
 import UIKit
 
@@ -42,12 +22,7 @@ public struct DawnTargetState {
     public var transform: CATransform3D?
     
     public enum Position {
-        case left
-        case right
-        case up
-        case down
-        case center
-        case any(x: CGFloat, y: CGFloat)
+        case left, right, up, down, center, any(x: CGFloat, y: CGFloat)
     }
     public var position: Position?
     
@@ -140,18 +115,6 @@ extension DawnModifier {
 }
 
 extension DawnModifier {
-    
-    public static func borderWidth(_ borderWidth: CGFloat) -> DawnModifier {
-        return DawnModifier { targetState in
-            targetState.borderWidth = borderWidth
-        }
-    }
-    
-    public static func borderColor(_ borderColor: UIColor) -> DawnModifier {
-        return DawnModifier { targetState in
-            targetState.borderColor = borderColor.cgColor
-        }
-    }
     
     public static func border(_ color: UIColor, width: CGFloat) -> DawnModifier {
         return DawnModifier { targetState in
